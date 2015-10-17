93cf3e064c8f80bf848db8a3248a196f0cfd523b
jazzdragon <climbinghobo@gmail.com>
2015-10-16 20:38:54 -0600

    init

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'jazzdragon:reactive-var-helper',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A helper to nicely organize your reactive vars',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.2');
+  api.use('ecmascript');
+  api.addFiles('reactive-var-helper.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('jazzdragon:reactive-var-helper');
+  api.addFiles('reactive-var-helper-tests.js');
+});
