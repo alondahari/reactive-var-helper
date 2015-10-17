b007fd1e97fa7d6872dbb45e13ee2fd277f01db8
jazzdragon <climbinghobo@gmail.com>
2015-10-16 20:40:02 -0600

    added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A helper to nicely organize your reactive vars',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jazzdragon/reactive-var-helper',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
